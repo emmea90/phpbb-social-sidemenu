31de937966c1c306c1db5f62489ef58bf71f5005
Alessio Massetti <alessiomassetti@gmail.com>
2016-03-23 17:30:52 +0100

    0.0.1 release

---
+++ language/it/socialsidemenu.php
@@ -0,0 +1,26 @@
+<?php
+/**
+*
+* @author emmea90
+* @package language
+* @copyright (c) 2013 emmea90
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// everywhere else
+$lang = array_merge($lang, array(
+));
