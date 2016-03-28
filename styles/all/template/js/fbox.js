3a981a2e704656d9c3163eb136241a3fdcce5a80
Alessio Massetti <alessiomassetti@gmail.com>
2016-03-28 14:21:52 +0200

    Social Side Menu en language + compatibility fixes

--- styles/all/template/js/fbox.js
+++ styles/all/template/js/fbox.js
@@ -1,8 +1,8 @@
 $(document).ready(function($) {
     if ($.cookie('popup_facebook_box') != 'yes') {
-        $('#fbox-background').delay(5000).fadeIn('medium');
-        $('#fbox-button, #fbox-close').click(function() {
-            $('#fbox-background').stop().fadeOut('medium');
+        $('.fbox-background').delay(5000).fadeIn('medium');
+        $('.fbox-button, .fbox-close, .theblogwidgets').click(function() {
+            $('.fbox-background').stop().fadeOut('medium');
         });
     }
     $.cookie('popup_facebook_box', 'yes', {
